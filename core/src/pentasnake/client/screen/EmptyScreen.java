d9f90b5c97949cf4fe0b15756fb0922fc851b9a6
Tom Ofella <spondeus@gmail.com>
2023-03-16 13:56:14 +0100

    Main menu tested with empty screen.

---
+++ core/src/pentasnake/client/screen/EmptyScreen.java
@@ -0,0 +1,61 @@
+package pentasnake.client.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import pentasnake.client.SnakeGame;
+
+public class EmptyScreen implements Screen {
+    SpriteBatch batch;
+    Texture img;
+
+    SnakeGame game;
+
+    public EmptyScreen(SnakeGame game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+        batch = new SpriteBatch();
+        img = new Texture("badlogic.jpg");
+    }
+
+    @Override
+    public void render(float delta) {
+        ScreenUtils.clear(1, 0, 0, 1);
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.end();
+        if(Gdx.input.isKeyPressed(Input.Keys.X)) game.setScreen(new MenuScreen(game));
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose () {
+        batch.dispose();
+        img.dispose();
+    }
+}
