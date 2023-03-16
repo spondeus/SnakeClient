803b68a7fc70aba0c2046d26e26830f70f3c020e
Hunor <ozshunor@gmail.com>
2023-03-16 12:28:21 +0100

    menuscreen with table

--- core/src/pentasnake/client/screen/MenuScreen.java
+++ core/src/pentasnake/client/screen/MenuScreen.java
@@ -6,23 +6,23 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
-import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 import pentasnake.client.SnakeGame;
 
 
 public class MenuScreen implements Screen {
 
-    private static final int BUTTON_WIDTH = 300;
-    private static final int BUTTON_HEIGHT = 100;
     private final SnakeGame game;
 
     private Stage stage;
-    OrthographicCamera camera = new OrthographicCamera();
-    FitViewport viewport = new FitViewport(800, 600, camera);
-    Button startButton, scoreButton, settingsButton;
+    private final OrthographicCamera camera = new OrthographicCamera();
+
+    private Viewport viewport;
+    private Table table;
     TextureAtlas atlas;
     Skin skin;
 
@@ -30,59 +30,49 @@ public class MenuScreen implements Screen {
         this.game = game;
     }
 
-    @Override
     public void show() {
-        stage = new Stage();
         atlas = new TextureAtlas(Gdx.files.internal("skin/uiskin.atlas"));
         skin = new Skin(Gdx.files.internal("skin/uiskin.json"), atlas);
         Texture snakeImg = new Texture(Gdx.files.internal("littleSnake.png"));
         Image image = new Image(snakeImg);
+        viewport = new ExtendViewport(1280,720);
+        stage = new Stage(viewport);
 
+        table = new Table();
+        table.setFillParent(true);
 
-        startButton = new Button(skin);
-        startButton.add("START (press S)");
-        startButton.setSize(BUTTON_WIDTH, BUTTON_HEIGHT);
-        startButton.setPosition(viewport.getScreenWidth() + 450, viewport.getScreenHeight() + 450);
-        startButton.addListener(new InputListener() {
-            @Override
-            public boolean touchDown(InputEvent ev, float x, float y, int p, int b) {
-                game.setScreen(new PlayScreen());
-                return true;
-            }
-        });
-
-
-        scoreButton = new Button(skin);
-        scoreButton.add("SCOREBOARD (press B)");
-        scoreButton.setSize(BUTTON_WIDTH, BUTTON_HEIGHT);
-        scoreButton.setPosition(viewport.getScreenWidth() + 450, viewport.getScreenHeight() + 300);
-        scoreButton.addListener(new InputListener() {
-            @Override
-            public boolean touchDown(InputEvent ev, float x, float y, int p, int b) {
-                game.setScreen(new ScoreboardScreen());
-                return true;
-            }
-        });
-
-        settingsButton = new Button(skin);
-        settingsButton.add("SETTIGNS (press T)");
-        settingsButton.setSize(BUTTON_WIDTH, BUTTON_HEIGHT);
-        settingsButton.setPosition(viewport.getScreenWidth() + 450, viewport.getScreenHeight() + 150);
-        settingsButton.addListener(new InputListener() {
-            @Override
-            public boolean touchDown(InputEvent ev, float x, float y, int p, int b) {
-//                game.setScreen(new SettingsScreen());
-                return true;
-            }
-        });
-
-
-        stage.addActor(startButton);
-        stage.addActor(scoreButton);
-        stage.addActor(settingsButton);
+        stage.addActor(table);
         stage.addActor(image);
+
+        addButton("Start").addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+//                game.setScreen(new PlayScreen());
+            }
+        });
+        addButton("Settings").addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+//                game.setScreen(new SettingsScreen());
+            }
+        });
+        addButton("Score").addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+//                game.setScreen(new ScoreboardScreen());
+            }
+        });
+
         Gdx.input.setInputProcessor(stage);
     }
+
+    private TextButton addButton(String name){
+        TextButton button = new TextButton(name, skin);
+        table.add(button).width(200).height(80).padBottom(10);
+        table.row();
+        return button;
+    }
+
 
     @Override
     public void render(float delta) {
@@ -97,8 +87,8 @@ public class MenuScreen implements Screen {
     }
 
     public void update(){
-        if (Gdx.input.isKeyPressed(Input.Keys.S)) game.setScreen(new PlayScreen());
-        if (Gdx.input.isKeyPressed(Input.Keys.B)) game.setScreen(new ScoreboardScreen());
+//        if (Gdx.input.isKeyPressed(Input.Keys.S)) game.setScreen(new PlayScreen());
+//        if (Gdx.input.isKeyPressed(Input.Keys.B)) game.setScreen(new ScoreboardScreen());
 //        if (Gdx.input.isKeyPressed(Input.Keys.T)) game.setScreen(new SettingsScreen());
     }
 
@@ -127,3 +117,4 @@ public class MenuScreen implements Screen {
         stage.dispose();
     }
 }
+
