8e22fd8efd2562032e48ed5c2ef48a8e41f94c85
OzsvathHunor <45239731+OzsvathHunor@users.noreply.github.com>
2023-03-16 10:28:54 +0100

    Settings button added

--- core/src/pentasnake/client/screen/MenuScreen.java
+++ core/src/pentasnake/client/screen/MenuScreen.java
@@ -9,12 +9,9 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
-import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
 import pentasnake.client.SnakeGame;
 
-import java.security.Key;
 
 public class MenuScreen implements Screen {
 
@@ -25,7 +22,7 @@ public class MenuScreen implements Screen {
     private Stage stage;
     OrthographicCamera camera = new OrthographicCamera();
     FitViewport viewport = new FitViewport(800, 600, camera);
-    Button startButton, scoreButton;
+    Button startButton, scoreButton, settingsButton;
     TextureAtlas atlas;
     Skin skin;
 
@@ -45,7 +42,7 @@ public class MenuScreen implements Screen {
         startButton = new Button(skin);
         startButton.add("START (press S)");
         startButton.setSize(BUTTON_WIDTH, BUTTON_HEIGHT);
-        startButton.setPosition(viewport.getScreenWidth() + 450, viewport.getScreenHeight() + 350);
+        startButton.setPosition(viewport.getScreenWidth() + 450, viewport.getScreenHeight() + 450);
         startButton.addListener(new InputListener() {
             @Override
             public boolean touchDown(InputEvent ev, float x, float y, int p, int b) {
@@ -58,7 +55,7 @@ public class MenuScreen implements Screen {
         scoreButton = new Button(skin);
         scoreButton.add("SCOREBOARD (press B)");
         scoreButton.setSize(BUTTON_WIDTH, BUTTON_HEIGHT);
-        scoreButton.setPosition(viewport.getScreenWidth() + 450, viewport.getScreenHeight() + 200);
+        scoreButton.setPosition(viewport.getScreenWidth() + 450, viewport.getScreenHeight() + 300);
         scoreButton.addListener(new InputListener() {
             @Override
             public boolean touchDown(InputEvent ev, float x, float y, int p, int b) {
@@ -67,9 +64,22 @@ public class MenuScreen implements Screen {
             }
         });
 
+        settingsButton = new Button(skin);
+        settingsButton.add("SETTIGNS (press T)");
+        settingsButton.setSize(BUTTON_WIDTH, BUTTON_HEIGHT);
+        settingsButton.setPosition(viewport.getScreenWidth() + 450, viewport.getScreenHeight() + 150);
+        settingsButton.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent ev, float x, float y, int p, int b) {
+//                game.setScreen(new SettingsScreen());
+                return true;
+            }
+        });
+
 
         stage.addActor(startButton);
         stage.addActor(scoreButton);
+        stage.addActor(settingsButton);
         stage.addActor(image);
         Gdx.input.setInputProcessor(stage);
     }
