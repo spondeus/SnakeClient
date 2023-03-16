c10c5624ccabc9e5fb551a42ad64e7e0fc3d8a7d
Tom Ofella <spondeus@gmail.com>
2023-03-16 18:13:39 +0100

    point arrangements corrected.

--- core/src/pentasnake/client/screen/PlayScreen.java
+++ core/src/pentasnake/client/screen/PlayScreen.java
@@ -1,4 +1,5 @@
 package pentasnake.client.screen;
+
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
@@ -20,7 +21,7 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-public class PlayScreen extends Game implements Screen,InputProcessor  { //InputProcessor
+public class PlayScreen extends Game implements Screen, InputProcessor { //InputProcessor
 
     private AssetManager assetManager;
     public Label.LabelStyle labelStyle;
@@ -31,102 +32,105 @@ public class PlayScreen extends Game implements Screen,InputProcessor  { //Input
     private ArrayList<Snake> snakeList;
     private Table table;
 
-    public PlayScreen()
-    {
+    public PlayScreen() {
         mainStage = new Stage();
         uiStage = new Stage();
         initialize();
     }
+
     public void initialize() {
         InputMultiplexer im = new InputMultiplexer();
-        Gdx.input.setInputProcessor( im );
+        Gdx.input.setInputProcessor(im);
         snakeList = new ArrayList<Snake>();
-        Snake snake = new  Snake(Gdx.graphics.getWidth()/2,Gdx.graphics.getHeight()/2,20, Color.GREEN);
+        Snake snake = new Snake(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2, 20, Color.GREEN);
         snakeList.add(snake);
         mainStage.addActor(snake);
         Gdx.input.setInputProcessor(new InputHandler(snake));
         labelInitialize();
     }
-    public void labelInitialize(){
+
+    public void labelInitialize() {
         labelStyle = new Label.LabelStyle();
-        labelStyle.font= new BitmapFont();
-        table =new Table();
-        labelStyle.fontColor=Color.GOLD;
-       // pointsLabel= new Label(snakeList.get(0).getPoints()+" p",labelStyle);
+        labelStyle.font = new BitmapFont();
+        table = new Table();
+        labelStyle.fontColor = Color.GOLD;
+        myPoints = new Label(snakeList.get(0).getPoints() + " p", labelStyle);
         pointsLabel = new LinkedList<>();
-        myPoints = new Label(snakeList.get(0).getPoints()+" p",labelStyle);
-        pointsLabel.add(myPoints);
-        pointsLabel.add(new Label ("10 p",labelStyle));
-        pointsLabel.add(new Label ("20 p",labelStyle));
-        pointsLabel.add(new Label ("30 p",labelStyle));
-        // pointsLabel.setPosition(Gdx.graphics.getWidth()-40,Gdx.graphics.getHeight()-30);
-        table.add(myPoints);
-        table.add().expandX();
-        table.add(pointsLabel.get(0)).row();
-        table.add();
-        table.add();
-        table.add(pointsLabel.get(1)).row();
-        table.add();
-        table.add();
-        table.add(pointsLabel.get(2)).row();
-        table.add();
-        table.add();
-        table.add(pointsLabel.get(3)).row();
-        table.add();
-        table.add();
+        pointsLabel.add(new Label(snakeList.get(0).getPoints() + " p", labelStyle));
+        pointsLabel.add(new Label("10 p", labelStyle));
+        pointsLabel.add(new Label("20 p", labelStyle));
+        pointsLabel.add(new Label("30 p", labelStyle));
+        Table pointTable = new Table();
+        for (Label label : pointsLabel) {
+            pointTable.add(label).row();
+        }
+        table.add(myPoints).top();
+        table.add().expand();
+        table.add(pointTable).top();
         table.setFillParent(true);
-       uiStage.addActor(table);
+        uiStage.addActor(table);
     }
 
     public AssetManager getAssetManager() {
         return assetManager;
     }
-    public  void update(float dt){}
-    public void render(float dt)
-    {
+
+    public void update(float dt) {
+    }
+
+    public void render(float dt) {
         uiStage.act(dt);
         mainStage.act(dt);
-        Gdx.gl.glClearColor(0,0,0,1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         mainStage.draw();
         uiStage.draw();
     }
+
     @Override
     public boolean keyDown(int keycode) {
         return false;
     }
+
     @Override
     public boolean keyUp(int keycode) {
         return false;
     }
+
     @Override
     public boolean keyTyped(char character) {
         return false;
     }
+
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         return false;
     }
+
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
         return false;
     }
+
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
         return false;
     }
+
     @Override
     public boolean mouseMoved(int screenX, int screenY) {
         return false;
     }
+
     @Override
     public boolean scrolled(float amountX, float amountY) {
         return false;
     }
+
     @Override
     public void show() {
-      //  InputMultiplexer im = (InputMultiplexer)Gdx.input.getInputProcessor();
-     InputMultiplexer   im = new InputMultiplexer();
+        //  InputMultiplexer im = (InputMultiplexer)Gdx.input.getInputProcessor();
+        InputMultiplexer im = new InputMultiplexer();
         im.addProcessor(this);
         im.addProcessor(uiStage);
         im.addProcessor(mainStage);
@@ -134,23 +138,30 @@ public class PlayScreen extends Game implements Screen,InputProcessor  { //Input
 
     @Override
     public void create() {
-        
+
     }
+
     @Override
-    public void resize(int width, int height) {}
+    public void resize(int width, int height) {
+    }
+
     @Override
-    public void pause() {}
+    public void pause() {
+    }
+
     @Override
     public void resume() {
 
     }
+
     @Override
     public void hide() {
-        InputMultiplexer im = (InputMultiplexer)Gdx.input.getInputProcessor();
+        InputMultiplexer im = (InputMultiplexer) Gdx.input.getInputProcessor();
         im.removeProcessor(this);
         im.removeProcessor(uiStage);
         im.removeProcessor(mainStage);
     }
+
     @Override
     public void dispose() {
     }
