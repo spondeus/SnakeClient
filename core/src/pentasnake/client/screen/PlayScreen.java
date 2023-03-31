8ebd9a7ef3f7451290ea8a574d58cd832ca01312
Tom Ofella <spondeus@gmail.com>
2023-03-31 20:49:24 +0200

    maybe now not parallel?

--- core/src/pentasnake/client/screen/PlayScreen.java
+++ core/src/pentasnake/client/screen/PlayScreen.java
@@ -287,8 +287,8 @@ public class PlayScreen implements Screen {
                 if (msg instanceof SnakeMove) {
                     SnakeMove snakeMove = (SnakeMove) msg;
                     Snake snake = snakeList.get(snakeMove.getId());
-                    if (snakeMove.isLeft()) snakeList.get(snakeMove.getId()).turnLeft();
-                    else snakeList.get(snakeMove.getId()).turnRight();
+                    if (snakeMove.isLeft()) snakeList.get(msg.getId()).turnLeft();
+                    else snakeList.get(msg.getId()).turnRight();
                 } else if (msg instanceof Pickup) putNewPickup((Pickup) msg);
                 else if (msg instanceof PickupRemove) removePickup((PickupRemove) msg);
 
